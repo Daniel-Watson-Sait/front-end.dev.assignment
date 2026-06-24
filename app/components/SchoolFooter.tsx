cc5a8d3d7742e4cd4b030aa0d13eb3efd19211fc
Daniel-Watson-Sait <daniel.watson1@edu.sait.ca>
2026-06-24 12:44:28 -0600

    small header/footer change

--- app/components/SchoolFooter.tsx
+++ app/components/SchoolFooter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer className='bg-white border-b border-gray-200 px-6 py-4'>
+      <footer className='bg-gray-200 border-t border-gray-400 px-6 py-4'>
       <div className='max-w-5xl mx-auto flex items-center justify-between'>
         <div className='text-lg font-bold text-gray-900'>
             <h1 className='text-lg font-bold text-gray-900'>New Generation High School</h1>
--- app/components/SchoolNavBar.tsx
+++ app/components/SchoolNavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <header className='bg-white border-b border-gray-200 px-6 py-4'>
+    <header className='bg-gray-200 border-b border-gray-400 px-6 py-4'>
       <div className='max-w-5xl mx-auto flex items-center justify-between'>
         <div className='text-lg font-bold text-gray-900'>
             <h1 className='text-lg font-bold text-gray-900'>Student Portal</h1>
