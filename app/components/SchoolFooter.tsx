43113884d94e3f997d0c156cf9c51da6bcff68ed
Daniel-Watson-Sait <daniel.watson1@edu.sait.ca>
2026-06-24 12:36:22 -0600

    initial commit

---
+++ app/components/SchoolFooter.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className='bg-white border-b border-gray-200 px-6 py-4'>
+      <div className='max-w-5xl mx-auto flex items-center justify-between'>
+        <div className='text-lg font-bold text-gray-900'>
+            <h1 className='text-lg font-bold text-gray-900'>New Generation High School</h1>
+        </div>
+      </div>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ app/components/SchoolNavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header className='bg-white border-b border-gray-200 px-6 py-4'>
+      <div className='max-w-5xl mx-auto flex items-center justify-between'>
+        <div className='text-lg font-bold text-gray-900'>
+            <h1 className='text-lg font-bold text-gray-900'>Student Portal</h1>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
